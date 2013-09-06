b317d1530b8e850a00306939196de1d1c8be712c
Matthew Brades <matthew.r.brades@googlemail.com>
2013-09-06 18:51:15 +0100

    Make updating easier

--- client/contrib/spendfrom/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='btcspendfrom',
-      version='1.0',
-      description='Command-line utility for parabola "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@parabolafoundation.org',
-      requires=['jsonrpc'],
-      scripts=['spendfrom.py'],
-      )
--- client/share/qt/make_windows_icon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/parabola.png
-ICON_DST=../../src/qt/res/icons/parabola.ico
-convert ${ICON_SRC} -resize 16x16 parabola-16.png
-convert ${ICON_SRC} -resize 32x32 parabola-32.png
-convert ${ICON_SRC} -resize 48x48 parabola-48.png
-convert parabola-16.png parabola-32.png parabola-48.png ${ICON_DST}
-
